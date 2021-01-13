9f989a43dfb38b847915e99d66c64d59fd51590f
Panyapaseuth <od_bigbang@github.com>
2021-01-13 22:46:47 +0700

    Category Post Mapping

--- src/main/java/com/pairlearning/expensetracker/domain/Category.java
+++ src/main/java/com/pairlearning/expensetracker/domain/Category.java
@@ -14,4 +14,44 @@ public class Category {
         this.description = description;
         this.totalExpense = totalExpense;
     }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getTotalExpense() {
+        return totalExpense;
+    }
+
+    public void setTotalExpense(Double totalExpense) {
+        this.totalExpense = totalExpense;
+    }
 }
